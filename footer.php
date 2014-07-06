bbb1bd2c12fd93bc0429bc27904596f3b5d1027d
Josh Pollock <jpollock412@gmail.com>
2014-07-06 17:28:09 -0400

    add filters in footer

--- footer.php
+++ footer.php
@@ -12,9 +12,17 @@
 
 			<footer id="colophon" class="site-footer <?php app_starter_footer_class(); ?>" role="contentinfo">
 				<div class="site-info <?php app_starter_site_info_class(); ?>">
+					<?php
+						if ( apply_filters( 'app_starter_default_site_info', false  ) ) :
+					?>
 					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'app_starter' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'app_starter' ), 'WordPress' ); ?></a>
 					<span class="sep"> | </span>
-					<?php printf( __( 'Theme: %1$s by %2$s.', 'app_starter' ), 'App Starter', '<a href="http://JoshPress.net/" rel="designer">Josh Pollock</a>' ); ?>
+					<?php printf( __( 'Theme: %1$s by %2$s.', 'app_starter' ), 'App Starter', '<a href="http://JoshPress.net/" rel="designer">Josh Pollock</a>' );
+					endif; //app_starter_default_site_info
+					if ( false !== ( $site_info = apply_filters( 'app_starter_site_info', false ) ) ) {
+						echo $site_info;
+					}
+					?>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		<a class="exit-off-canvas"></a>
