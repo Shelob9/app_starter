21b4f152c9287575cbef82e85fcb82c5b00049ee
Josh Pollock <jpollock412@gmail.com>
2014-08-13 18:13:28 -0400

    Add 'app_starter_alt_main_view' filter

--- index.php
+++ index.php
@@ -16,69 +16,98 @@ app_starter_header(); ?>
 <div id="primary" class="content-area <?php app_starter_primary_class(); ?>">
 	<main id="main" class="site-main <?php app_starter_main_class(); ?>" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+		/**
+		 * Filter to use some content <em>instead of the loop</em>.
+		 *
+		 * @params bool|string False to not use or a string to echo, or a file path to include (via pods_view() or include()
+		 *
+		 * @return The view or false to use the loop normally.
+		 *
+		 * @since 0.0.2
+		 */
+		$alt_view = apply_filters( 'app_starter_alt_main_view', false );
+		if ( ! $alt_view ) {
+			if ( have_posts() ) :
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				/**
-				 * Override what content part is used for main item view
-				 *
-				 * @param string|null $view Path to the view relative to theme/child theme or the complete content to echo.
-				 *
-				 * @return file name.
-				 *
-				 * @since 0.0.2
-				 */
-				$view = apply_filters( 'app_starter_content_part_view', null );
+				while ( have_posts() ) : the_post();
 
-				if ( is_null( $view ) ) {
-					$type = get_post_type();
-					if ( $type === FALSE || $type === 'post' || ( is_singular( $type ) && !file_exists( 'content-' . $type ) ) ) {
-						$type = 'single';
-					}
-
-					get_template_part( 'content', $type );
-				}
-				else {
 					/**
-					 * Filter to prevent theme from attempting to use pods_view() for the
+					 * Override what content part is used for main item view
+					 *
+					 * @param string|null $view Path to the view relative to theme/child theme or the complete content to echo.
+					 *
+					 * @return file name.
 					 *
 					 * @since 0.0.2
-					 *
-					 * @param bool True to bypass, false to attempt to use it.
 					 */
-					$bypass_pods_view = apply_filters( 'app_starter_bypass_pods_view_in_index', false );
-					if ( $bypass_pods_view && function_exists( 'pods_view' ) && is_file( $view ) ) {
-						pods_view( $view, null, app_starter_cache_expires(), app_starter_cache_mode() );
+					$view = apply_filters( 'app_starter_content_part_view', null );
+
+					if ( is_null( $view ) ) {
+						$type = get_post_type();
+						if ( $type === false || $type === 'post' || ( is_singular( $type ) && ! file_exists( 'content-' . $type ) ) ) {
+							$type = 'single';
+						}
+
+						get_template_part( 'content', $type );
 					}
 					else {
-
-						if ( is_file( $view ) ) {
-							if ( file_exists(trailingslashit( get_stylesheet_directory() ).$view  ) ) {
-								$view = trailingslashit( get_stylesheet_directory() ).$view;
-							}
-							include( $view );
+						/**
+						 * Filter to prevent theme from attempting to use pods_view() for the
+						 *
+						 * @since 0.0.2
+						 *
+						 * @param bool True to bypass, false to attempt to use it.
+						 */
+						$bypass_pods_view = apply_filters( 'app_starter_bypass_pods_view_in_index', false );
+						if ( $bypass_pods_view && function_exists( 'pods_view' ) && is_file( $view ) ) {
+							pods_view( $view, null, app_starter_cache_expires(), app_starter_cache_mode() );
 						}
 						else {
-							echo $view;
+
+							if ( is_file( $view ) ) {
+								if ( file_exists( trailingslashit( get_stylesheet_directory() ) . $view ) ) {
+									$view = trailingslashit( get_stylesheet_directory() ) . $view;
+								}
+								include( $view );
+							}
+							else {
+								echo $view;
+							}
+
 						}
 
 					}
 
+
+				endwhile;
+
+				app_starter_paging_nav();
+
+			else :
+
+			get_template_part( 'content', 'none' );
+
+			endif;
+		}
+		else {
+			if ( is_file( $alt_view ) && file_exists( $alt_view )  ) {
+				if ( function_exists( 'pods_view' ) ) {
+					pods_view( $alt_view, null, app_starter_cache_expires(), app_starter_cache_mode() );
 				}
-				?>
+				else {
+					include ( $alt_view );
+				}
+			}
+			else {
+				if ( is_string( $alt_view ) ) {
+					echo $alt_view;
+				}
+			}
+		}
 
-			<?php endwhile; ?>
-
-			<?php app_starter_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
+		?>
 
 	</main><!-- #main -->
 	<?php app_starter_sidebar(); ?>
